50fc1538c957609f06fa34f1735633655147efec
Marco <maphy1707@gmail.com>
2022-01-14 21:09:35 -0600

    Ejemplos de arreglos

---
+++ Arreglos.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int miArreglo[5];
+	
+	miArreglo[0] = 4;
+	miArreglo[1] = 5;
+	miArreglo[2] = 8;
+	miArreglo[3] = 6;
+	miArreglo[4] = 2;
+	
+	printf("%i %i %i %i %i", miArreglo[0], miArreglo[1], miArreglo[2], miArreglo[3], miArreglo[4]);
+	
+	char miNombre[5];
+	
+	miNombre[0] = 'M';
+	miNombre[1] = 'A';
+	miNombre[2] = 'R';
+	miNombre[3] = 'C';
+	miNombre[4] = 'O';
+	
+	printf("\n%c %c %c %c %c", miNombre[0], miNombre[1], miNombre[2], miNombre[3], miNombre[4]);
+	
+	/* 
+	* 
+	1- Buscar la forma de que mi arreglo y mi nombre se pinte con un for
+	* 
+	2- 
+	*
+	**
+	***
+	*****
+	******
+	
+	3- qUE LE PIDAS AL USUARIO EL NUMERO DE LINEAS DEL TRIANGULO 
+	
+	hACER UN JUEGO EN DONDE EN MEMORIA ESTE GUARDA UNA PALABRA, EL USUARIO TIENE 3 INTENTOS
+	
+	*/
+	
+	return 0;
+}
